dcbd2e0c21fa132b1031c05aaf7307671bfbcea3
Nox <xxevilmarcelxx@gmail.com>
2023-03-26 22:43:19 -0400

    Reorder mods

--- b1.1_02/src/main/java/cc/noxiuam/titanic/client/module/ModuleManager.java
+++ b1.1_02/src/main/java/cc/noxiuam/titanic/client/module/ModuleManager.java
@@ -37,15 +37,16 @@ public class ModuleManager {
     public ModuleManager() {
         // qol mods
         this.mods.add(this.coordinatesMod = new CoordinatesMod());
-        this.mods.add(this.chatEditorModule = new ChatEditorModule());
-        this.mods.add(this.perspectiveModule = new PerspectiveModule());
-        this.mods.add(this.menuTweaksModule = new MenuTweaksModule());
         this.mods.add(this.modernInventoryModule = new ModernInventoryModule());
-        this.mods.add(this.autoLoginModule = new AutoLoginModule());
-        this.mods.add(this.nametagEditorModule = new NametagEditorModule());
-        this.mods.add(this.performanceModule = new PerformanceModule());
         this.mods.add(this.screenshotModule = new ScreenshotModule());
+        this.mods.add(this.performanceModule = new PerformanceModule());
         this.mods.add(this.zoomModule = new ZoomModule());
+
+        this.mods.add(this.autoLoginModule = new AutoLoginModule());
+        this.mods.add(this.menuTweaksModule = new MenuTweaksModule());
+        this.mods.add(this.perspectiveModule = new PerspectiveModule());
+        this.mods.add(this.chatEditorModule = new ChatEditorModule());
+        this.mods.add(this.nametagEditorModule = new NametagEditorModule());
 
         // game fixes
         this.mods.add(new PlayerAssetFix());
