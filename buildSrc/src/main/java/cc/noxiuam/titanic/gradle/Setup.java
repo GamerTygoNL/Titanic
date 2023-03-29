3458c3dd2558fc05560ef78487a9175f559df8d0
TheKodeToad <TheKodeToad@proton.me>
2023-03-29 22:16:33 +0100

    format ;p

--- buildSrc/src/main/java/cc/noxiuam/titanic/gradle/Setup.java
+++ buildSrc/src/main/java/cc/noxiuam/titanic/gradle/Setup.java
@@ -15,103 +15,98 @@ import org.mcphackers.mcp.tools.versions.json.Version;
 
 public class Setup extends MCP implements Action<Task> {
 
-	private final TitanicGradlePlugin plugin;
-	private final Logger logger;
-	private final Options options = new Options();
-	private Version version;
+    private final TitanicGradlePlugin plugin;
+    private final Logger logger;
+    private final Options options = new Options();
+    private Version version;
 
-	public Setup(TitanicGradlePlugin plugin) {
-		this.plugin = plugin;
-		this.logger = plugin.getProject().getLogger();
-		options.setParameter(TaskParameter.SETUP_VERSION, plugin.getVersion());
-	}
+    public Setup(TitanicGradlePlugin plugin) {
+        this.plugin = plugin;
+        this.logger = plugin.getProject().getLogger();
+        options.setParameter(TaskParameter.SETUP_VERSION, plugin.getVersion());
+    }
 
-	@Override
-	public void execute(Task task) {
-		log("Downloading...");
-		performTask(TaskMode.SETUP, Side.CLIENT);
-		log("Decompiling...");
-		performTask(TaskMode.DECOMPILE, Side.CLIENT);
-	}
+    @Override
+    public void execute(Task task) {
+        log("Downloading...");
+        performTask(TaskMode.SETUP, Side.CLIENT);
+        log("Decompiling...");
+        performTask(TaskMode.DECOMPILE, Side.CLIENT);
+    }
 
-	@Override
-	public void clearProgressBars() {
-	}
+    @Override
+    public void clearProgressBars() {}
 
-	@Override
-	public Version getCurrentVersion() {
-		return version;
-	}
+    @Override
+    public Version getCurrentVersion() {
+        return version;
+    }
 
-	@Override
-	public Options getOptions() {
-		return options;
-	}
+    @Override
+    public Options getOptions() {
+        return options;
+    }
 
-	@Override
-	public Path getWorkingDir() {
-		return plugin.getWork();
-	}
+    @Override
+    public Path getWorkingDir() {
+        return plugin.getWork();
+    }
 
-	@Override
-	public String inputString(String arg0, String arg1) {
-		throw new UnsupportedOperationException("Input was requested");	}
+    @Override
+    public String inputString(String arg0, String arg1) {
+        throw new UnsupportedOperationException("Input was requested");
+    }
 
-	@Override
-	public void log(String msg) {
-		logger.quiet(msg);
-	}
+    @Override
+    public void log(String msg) {
+        logger.quiet(msg);
+    }
 
-	@Override
-	public void setActive(boolean active) {
-	}
+    @Override
+    public void setActive(boolean active) {}
 
-	@Override
-	public void setCurrentVersion(Version version) {
-		this.version = version;
-	}
+    @Override
+    public void setCurrentVersion(Version version) {
+        this.version = version;
+    }
 
-	@Override
-	public void setProgress(int arg0, String arg1) {
-	}
+    @Override
+    public void setProgress(int arg0, String arg1) {}
 
-	@Override
-	public void setProgress(int arg0, int arg1) {
-	}
+    @Override
+    public void setProgress(int arg0, int arg1) {}
 
-	@Override
-	public void setProgressBars(List<org.mcphackers.mcp.tasks.Task> arg0, TaskMode arg1) {
-	}
+    @Override
+    public void setProgressBars(List<org.mcphackers.mcp.tasks.Task> arg0, TaskMode arg1) {}
 
-	@Override
-	public void showMessage(String title, String msg, int arg2) {
-		switch (arg2) {
-		case 0:
-			logger.quiet(msg);
-			break;
-		case 1:
-			logger.warn(msg);
-			break;
-		case 2:
-			logger.error(msg);
-			break;
-		}
-	}
+    @Override
+    public void showMessage(String title, String msg, int arg2) {
+        switch (arg2) {
+            case 0:
+                logger.quiet(msg);
+                break;
+            case 1:
+                logger.warn(msg);
+                break;
+            case 2:
+                logger.error(msg);
+                break;
+        }
+    }
 
-	@Override
-	public void showMessage(String title, String msg, Throwable error) {
-		logger.error(title, msg, error);
-	}
+    @Override
+    public void showMessage(String title, String msg, Throwable error) {
+        logger.error(title, msg, error);
+    }
 
-	@Override
-	public boolean updateDialogue(String changelog, String version) {
-		logger.warn("Outdated version. Update to {} available!", version);
-		return false;
-	}
+    @Override
+    public boolean updateDialogue(String changelog, String version) {
+        logger.warn("Outdated version. Update to {} available!", version);
+        return false;
+    }
 
-	@Override
-	public boolean yesNoInput(String title, String msg) {
-		throw new UnsupportedOperationException("Input was requested");
-	}
-
+    @Override
+    public boolean yesNoInput(String title, String msg) {
+        throw new UnsupportedOperationException("Input was requested");
+    }
 }
--- buildSrc/src/main/java/cc/noxiuam/titanic/gradle/TitanicGradlePlugin.java
+++ buildSrc/src/main/java/cc/noxiuam/titanic/gradle/TitanicGradlePlugin.java
@@ -9,40 +9,39 @@ import org.gradle.api.Project;
 
 public class TitanicGradlePlugin implements Plugin<Project> {
 
-	private Project project;
-	private String version;
-	private Path work;
+    private Project project;
+    private String version;
+    private Path work;
 
-	@Override
-	public void apply(Project project) {
-		if (this.project != null)
-			throw new IllegalStateException("owo");
-		this.project = project;
+    @Override
+    public void apply(Project project) {
+        if (this.project != null)
+            throw new IllegalStateException("owo");
+        this.project = project;
 
-		version = project.property("minecraft_version").toString();
-		work = project.getProjectDir().toPath().resolve("mcp");
+        version = project.property("minecraft_version").toString();
+        work = project.getProjectDir().toPath().resolve("mcp");
 
-		if (!Files.isDirectory(work)) {
-			try {
-				Files.createDirectories(work);
-			} catch (IOException error) {
-				throw new IllegalStateException(error);
-			}
-		}
+        if (!Files.isDirectory(work)) {
+            try {
+                Files.createDirectories(work);
+            } catch (IOException error) {
+                throw new IllegalStateException(error);
+            }
+        }
 
-		project.task("setup", task -> task.doLast(new Setup(this)));
-	}
+        project.task("setup", task -> task.doLast(new Setup(this)));
+    }
 
-	public Project getProject() {
-		return project;
-	}
+    public Project getProject() {
+        return project;
+    }
 
-	public String getVersion() {
-		return version;
-	}
+    public String getVersion() {
+        return version;
+    }
 
-	public Path getWork() {
-		return work;
-	}
-
+    public Path getWork() {
+        return work;
+    }
 }
